b1b12200e0b5aa7f7338fb01ed3efbfeaeba8b65
92andrew.brady@gmail.com <92andrew.brady@gmail.com>
2018-05-26 22:46:18 -0300

    Added a for loop to loop through given twitch users

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function twitchUser()  {
-    var url = 'https://wind-bow.gomix.me/twitch-api/users/freecodecamp';
-    $.ajax({
-        url: url,
-        dataType: 'jsonp',
-        success: function(data) {
-            console.log(data);
-        }
-    })
+    var url = 'https://wind-bow.gomix.me/twitch-api/users/';
+    var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+    for (var i = 0; i < users.length; i++) {
+        $.ajax({
+            url: url + users[i],
+            dataType: 'jsonp',
+            success: function(data) {
+                console.log(data);
+            }
+        })
+    }
 }
 
 $(document).ready(function() {
