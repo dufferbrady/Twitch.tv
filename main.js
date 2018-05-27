12cd351985dea8102ecb0549d4ddc61b6e452ae9
92andrew.brady@gmail.com <92andrew.brady@gmail.com>
2018-05-27 13:21:59 -0300

    configured userData function to show if channel is online or not

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function twitchUser()  {
-    var url = 'https://wind-bow.gomix.me/twitch-api/users/';
-    var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+    var url = 'https://wind-bow.gomix.me/twitch-api/channels/';
+    var users = ["ESL_SC2", "Lobosjr", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
     for (var i = 0; i < users.length; i++) {
         $.ajax({
-            url: url + users[i],
+            url: url + users[i] + '?callback=?',
             dataType: 'jsonp',
             success: function(data) {
-                // console.log(data);
+                console.log(data);
                 userData(data);
             }
         })
@@ -14,18 +14,27 @@ function twitchUser()  {
 }
 
 function userData(info) {
+    var status;
+    if(info.partner == true) {
+        status = 'Online';
+    } else {
+        status = 'Offline';
+    }
     const container = document.getElementsByClassName('container')[0];
     const item = document.createElement('div')
     const h1 = document.createElement('h1');
     const logo = document.createElement('img');
+    const mode = document.createElement('div');
 
     item.setAttribute('class', 'item');
+    mode.textContent = status;
     h1.textContent = info.display_name;
     logo.setAttribute('src', info.logo);
 
     container.appendChild(item);
     item.appendChild(logo)
     item.appendChild(h1)
+    item.appendChild(mode)
 }
 
 
